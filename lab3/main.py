d305be530daaa81838cc1d04727fe2db0980bff8
haydenkkk <lihaoxin67@gmail.com>
2023-05-24 23:55:08 +0800

    update

--- lab3/main.py
+++ lab3/main.py
@@ -1,12 +1,13 @@
 import spacy
 
 nlp = spacy.load("en_core_web_sm")
+
 def process_document(doc_text):
     doc = nlp(doc_text)
-
     # 构建NER结构
     ner_structure = []
     for ent in doc.ents:
+        print(ent.root.text, ent.root.dep_, ent.root.head.text)
         ner_structure.append({
             "text": ent.text,
             "label": ent.label_,
@@ -16,11 +17,13 @@ def process_document(doc_text):
     return ner_structure
 
 # 遍历每个文档进行处理
-for doc_path in document_paths:
-    with open(doc_path, "r") as f:
-        doc_text = f.read()
+# for doc_path in document_paths:
+#     with open(doc_path, "r") as f:
+#         doc_text = f.read()
 
-    ner_structure = process_document(doc_text)
+#     ner_structure = process_document(doc_text)
 
-    # 将NER结构保存到文件或数据库中
-    save_ner_structure(ner_structure)
+#     # 将NER结构保存到文件或数据库中
+#     save_ner_structure(ner_structure)
+process_document("Apple is looking at buying U.K. startup for $1 billion")
+# print(process_document("Apple is looking at buying U.K. startup for $1 billion"))
