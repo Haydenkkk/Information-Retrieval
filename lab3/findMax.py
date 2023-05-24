06105bcab3f0283677d94476a7e6afc4f8b5c27a
haydenkkk <lihaoxin67@gmail.com>
2023-05-25 03:11:11 +0800

    实现对每篇文章最高关键信息的提取和保存

---
+++ lab3/findMax.py
@@ -0,0 +1,59 @@
+import spacy
+import json
+
+# 加载spaCy模型
+nlp = spacy.load("en_core_web_sm")
+
+# 打开JSON文件
+with open('CNN_Articels_clean.json', 'r', encoding='utf-8') as file:
+    data = json.load(file)
+
+# 遍历JSON中的每个对象
+for obj in data:
+    # 获取"Article text"字段的值
+    text = obj["Article text"]
+
+    # 使用spaCy处理文本
+    doc = nlp(text)
+
+    # 统计实体类型中每个token的出现次数
+    # PERSON：人名
+    # NORP：民族、宗教或政治团体
+    # FAC：建筑物、机构、机场等
+    # ORG：组织机构、公司、政府等
+    # GPE：地点、国家、城市等
+    # LOC：非GPE地点，山脉、湖泊等
+    # PRODUCT：产品名称
+    # EVENT：事件名称
+    # WORK_OF_ART：艺术作品，书籍、歌曲等
+    # LAW：法律文件名
+    # LANGUAGE：语言名称
+    # DATE：日期
+    # TIME：时间
+    # PERCENT：百分比
+    # MONEY：货币金额
+    # QUANTITY：数量
+    # ORDINAL：顺序词，第一、第二等
+    # CARDINAL：基数词，数字
+    entity_counts = {
+        "PERSON": {}, "NORP": {}, "FAC": {}, "ORG": {}, "GPE": {}, "LOC": {}, "PRODUCT": {},
+        "EVENT": {}, "WORK_OF_ART": {}, "LANGUAGE": {}, "DATE": {}, "TIME": {}, "MONEY": {}
+    }
+    for ent in doc.ents:
+        if ent.label_ in entity_counts:
+            token_counts = entity_counts[ent.label_]
+            token_counts[ent.text] = token_counts.get(ent.text, 0) + 1
+
+    # 获取每个实体类型中出现频率最高的两个token
+    top_tokens = {}
+    for label, counts in entity_counts.items():
+        sorted_counts = sorted(counts.items(), key=lambda x: x[1], reverse=True)
+        top_tokens[label] = {token: count for token, count in sorted_counts[:2] if count > 1}
+
+    # 将处理结果添加到JSON对象中
+    obj["Top Tokens"] = top_tokens
+    print(f"已处理: {obj['Headline']}")
+    print(f"Top Tokens: {top_tokens}")
+# 将更新后的JSON保存到文件
+with open('res.json', 'w', encoding='utf-8') as file:
+    json.dump(data, file, indent=4)
--- lab3/main.py
+++ lab3/main.py
@@ -1,14 +1,26 @@
 import spacy
 from spacy import displacy
+import os
 
 nlp = spacy.load("en_core_web_sm")
-text="Binary weights for the part-of-speech tagger, dependency parser and named entity recognizer to predict those annotations in context."
-doc = nlp(text)
 
 def process_document(doc_text):
     doc = nlp(doc_text)
     # 构建NER结构
     ner_structure = []
+    entity_counts = {
+                    "PERSON": {}, "NORP":{} , "FAC": {}, "ORG": {}, "GPE": {}, "LOC": {}, "PRODUCT": {},
+                    "EVENT": {}, "WORK_OF_ART": {}, "LANGUAGE": {}, "DATE": {}, "TIME": {}, "MONEY": {}
+                }
+    for ent in doc.ents:
+        if ent.label_ in entity_counts:
+            entity_counts[ent.label_][ent.text] = entity_counts[ent.label_].get(ent.text, 0) + 1
+
+    top_tokens = {}
+    for label, counts in entity_counts.items():
+        sorted_counts = sorted(counts.items(), key=lambda x: x[1], reverse=True)
+        top_tokens[label] = sorted_counts[:2]
+        
     for ent in doc.ents:
         print(ent.text, ent.label_)
         ner_structure.append({
@@ -19,14 +31,18 @@ def process_document(doc_text):
         })
     return ner_structure
 
-# 遍历每个文档进行处理
-# for doc_path in document_paths:
-#     with open(doc_path, "r") as f:
-#         doc_text = f.read()
+current_folder = os.getcwd()
+folder_path = os.path.join(current_folder, "docs")
+for file_name in os.listdir(folder_path):
+    # 构建文件的完整路径
+    file_path = os.path.join(folder_path, file_name)
+    # 检查文件是否为普通文件
+    if os.path.isfile(file_path):
+        # 读取文件内容
+        with open(file_path, "r", encoding="utf-8") as f:
+            text = f.read()
+        # 使用spaCy处理文件内容
+        ner_structure = process_document(text)
 
-#     ner_structure = process_document(doc_text)
 
-#     # 将NER结构保存到文件或数据库中
-#     save_ner_structure(ner_structure)
-print(process_document(text))
-displacy.serve(doc, style="ent")
+# displacy.serve(doc, style="ent")
