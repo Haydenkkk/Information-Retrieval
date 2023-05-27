a30c9efd6ff780f3dfcb59135847aa0c66a5480a
Gooose <96369401+Haydenkkk@users.noreply.github.com>
2023-05-27 11:51:50 +0800

    Create App.jsx

---
+++ lab3/frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles.css";
+import Home from "./Home";
+
+function App() {
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
